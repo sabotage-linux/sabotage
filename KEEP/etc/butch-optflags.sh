144666e958c503df08cc3e229eb977a1cba19d64
rofl0r <retnyg@gmx.net>
2015-07-04 19:35:10 +0100

    butch-optflags.sh: test for simple flags first and return if set

--- KEEP/etc/butch-optflags.sh
+++ KEEP/etc/butch-optflags.sh
@@ -3,6 +3,17 @@
 # put your custom cflags and ldflags here...
 # this file is sourced by the default butch build template
 
+if [ "$DEBUGBUILD" = "1" ] ; then
+	# use "DEBUGBUILD=1 butch install mypkg" to create debug version of mypkg
+	optcflags="-O0 -g3"
+	optldflags=
+	return 0
+elif [ "$TESTBUILD" = "1" ] ; then
+	optcflags="-O0 -g0"
+	optldflags=
+	return 0
+fi
+
 isgcc3() {
 	local mycc="$CC"
 	[ -z "$mycc" ] && mycc=gcc
@@ -11,15 +22,6 @@ isgcc3() {
 
 optcflags="-fdata-sections -ffunction-sections -Os -g0 -fno-unwind-tables -fno-asynchronous-unwind-tables -Wa,--noexecstack"
 optldflags="-s -Wl,--gc-sections -Wl,-z,relro,-z,now"
-
-if [ "$DEBUGBUILD" = "1" ] ; then
-	# use "DEBUGBUILD=1 butch install mypkg" to create debug version of mypkg
-	optcflags="-O0 -g3"
-	optldflags=
-elif [ "$TESTBUILD" = "1" ] ; then
-	optcflags="-O0 -g0"
-	optldflags=
-fi
 
 if [ "$BRUTE" = 2 ] ; then
 	optcflags="$optcflags -s -Os -flto -fwhole-program"
