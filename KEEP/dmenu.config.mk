e6d87f315dc9c1057e4b43910d3d0e61ad55f2ee
rofl0r <retnyg@gmx.net>
2017-02-01 16:50:27 +0000

    fix dmenu
    package and config was made quite unskillfully with a lot of hardcoded paths,
    and failed to find freetype headers, and was broken for x-compile as well.

--- KEEP/dmenu.config.mk
+++ KEEP/dmenu.config.mk
@@ -20,7 +20,7 @@ LIBS = -L${X11LIB} -lfontconfig -lXft -lX11 -lxcb -lXau -lXdmcp ${XINERAMALIBS}
 CPPFLAGS = -D_BSD_SOURCE -D_POSIX_C_SOURCE=2 -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
 #CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
 CFLAGS   = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
-LDFLAGS  = -s ${LIBS}
+LDFLAGS  = ${LIBS}
 
 # compiler and linker
 CC = cc
