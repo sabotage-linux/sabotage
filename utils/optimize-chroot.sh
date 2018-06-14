70eb71115360901f1e2adff23fba86bdec624e7f
JillTyrell <jt@deadlytowers.org>
2018-06-14 08:25:26 +0000

    optimize-chroot.sh: Add missing packages; update gcc.

--- utils/optimize-chroot.sh
+++ utils/optimize-chroot.sh
@@ -2,6 +2,6 @@
 # use: optimize-chroot.sh [no args]
 # Rebuilds the stage0/1 binaries with the stage 1 compiler.
 
-butch rebuild jobflow patch busybox binutils make \
-	m4 gmp mpfr mpc libz libelf gcc630 \
-	musl 9base pkgconf libblkid e2fsprogs join libressl
+butch rebuild jobflow sabotage-core patch busybox binutils make \
+	kbd m4 gmp mpfr mpc libz libelf gcc640 musl 9base pkgconf \
+	libblkid e2fsprogs man join libressl ca-certificates
