1373107624bba5e5b302abb5e3abfe0b44ae20aa
Crystal Jelly <AequoreaVictoria@users.noreply.github.com>
2019-08-07 20:05:27 +0000

    nodejs: Updating to 12.7.0
    In order to preserve 'npm install --global' functionality, a
    'npm-fix' script has been included in /bin. After updating npm via
    the 'npm i -g npm' command, you should run 'npm-fix' to reapply
    the patch.

--- KEEP/nodejs-openssl_termios.h
+++ KEEP/nodejs-openssl_termios.h
@@ -1,10 +1,10 @@
---- node-v0.10.2.org/deps/openssl/openssl/crypto/ui/ui_openssl.c	2013-04-01 22:15:33.447000003 +0000
-+++ node-v0.10.2/deps/openssl/openssl/crypto/ui/ui_openssl.c	2013-04-01 22:40:28.785000003 +0000
-@@ -147,6 +147,7 @@
- # if defined(_POSIX_VERSION)
+--- a/deps/openssl/openssl/crypto/ui/ui_openssl.c
++++ b/deps/openssl/openssl/crypto/ui/ui_openssl.c
+@@ -46,6 +46,7 @@
+ #  if defined(_POSIX_VERSION) && _POSIX_VERSION>=199309L
  
- #  define SIGACTION
-+#  undef TERMIO
- #  if !defined(TERMIOS) && !defined(TERMIO) && !defined(SGTTY)
- #   define TERMIOS
- #  endif
+ #   define SIGACTION
++#   undef TERMIO
+ #   if !defined(TERMIOS) && !defined(TERMIO) && !defined(SGTTY)
+ #    define TERMIOS
+ #   endif
