8d39e302c1679e1272c6f0dd5f880dc61a92312d
rofl0r <rofl0r@users.noreply.github.com>
2019-08-16 23:33:16 +0100

    butch-optflags: add speed template
    env var OPT_SPEED activates "optimize for speed"
    env var OPT_KEEP_DEBUG adds -g to CFLAGS and removes -s from LDFLAGS
    (debug info is automatically split into separate files via the
    butch build template).

--- KEEP/etc/butch-optflags.sh
+++ KEEP/etc/butch-optflags.sh
@@ -24,12 +24,29 @@ case $A in i?86) isx86=1;; esac
 isgcc3=0
 [ "$STAGE" = 0 ] || is_gcc3 && isgcc3=1
 
-optcflags="-fdata-sections -ffunction-sections -Os -g0 -fno-unwind-tables -fno-asynchronous-unwind-tables -Wa,--noexecstack"
-optldflags="-s -Wl,--gc-sections -Wl,-z,relro,-z,now -Wl,-z,text"
+cflags_base="-fno-unwind-tables -fno-asynchronous-unwind-tables -Wa,--noexecstack"
+cflags_size="-Os -g0 -fdata-sections -ffunction-sections"
+cflags_speed="-O3 -fstrength-reduce -fthread-jumps -fcse-follow-jumps -fcse-skip-blocks -frerun-cse-after-loop -fexpensive-optimizations -fforce-addr -fomit-frame-pointer"
+
+ldflags_base="-Wl,-z,relro,-z,now -Wl,-z,text"
+ldflags_size="-Wl,--gc-sections"
+
+if [ "$OPT_KEEP_DEBUG" = 1 ] ; then
+	cflags_base="$cflags_base -g"
+else
+	ldflags_base="$ldflags_base -s"
+fi
+
+optcflags="$cflags_size $cflags_base"
+optldflags="$ldflags_size $ldflags_base"
+
 [ "$isx86" = 1 ] || [ "$A" = x86_64 ] && [ "$isgcc3" = 0 ] && \
 	optcflags="$optcflags -mtune=generic"
 
-if [ "$BRUTE" = 2 ] ; then
+if [ "$OPT_SPEED" = 1 ] ; then
+	optcflags="$cflags_base $cflags_speed -mtune=generic"
+
+elif [ "$BRUTE" = 2 ] ; then
 	optcflags="$optcflags -s -Os -flto -fwhole-program"
 	optldflags="$optldflags -flto -fwhole-program"
 elif [ "$BRUTE" = 1 ] ; then
