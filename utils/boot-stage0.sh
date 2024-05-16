9f3291d17c10bd1155f9edd1969ab02cb1720a0e
David A Roberts <d@vidr.cc>
2024-05-16 19:28:43 +1000

    Combine bootstrap scripts

--- utils/boot-stage0.sh
+++ utils/boot-stage0.sh
@@ -7,7 +7,7 @@ MAKE_THREADS=$(nproc)
 
 cd /root
 
-make gawk binutils bzip2 curl linux-headers
+make -s gawk binutils bzip2 curl linux-headers
 
 mkdir -p /sabotage
 ln -s /src /sabotage/src
@@ -17,11 +17,30 @@ cp KEEP/config.stage0 config
 sed -e "s@SABOTAGE_BUILDDIR=.*@SABOTAGE_BUILDDIR=/sabotage@" \
     -e "s@CC=.*@CC=cc@" -e "s@HOSTCC=.*@HOSTCC=cc@" \
     -e "s@MAKE_THREADS=1@MAKE_THREADS=$MAKE_THREADS@" \
-    -i config && cat config
+    -i config
 
 utils/setup-rootfs.sh
 
 CONFIG=/sabotage/src/config BUTCHDB=/sabotage/var/lib/butch.db DEPS=build:stage0 KEEP/bin/butch install stage0
 
 echo "export A=$ARCH" > config && cat KEEP/config.stage1 >> config
-sed -i "s@MAKE_THREADS=1@MAKE_THREADS=$MAKE_THREADS@" config && cat config
+sed -i "s@MAKE_THREADS=1@MAKE_THREADS=$MAKE_THREADS@" config
+
+# Replace the rootfs with the one we just built
+
+cd /
+rm -rf root include lib local tmp usr
+rm $(find /bin -type f -not -name sh)
+
+[ -e /etc/hostname ] && mv /sabotage/etc/hostname /etc/hostname.bak
+[ -e /etc/hosts ] && mv /sabotage/etc/hosts /etc/hosts.bak
+[ -e /etc/resolv.conf ] && mv /sabotage/etc/resolv.conf /etc/resolv.conf.bak
+
+mv /sabotage/bin/* /bin
+mv /sabotage/etc/* /etc
+
+rm /sabotage/src
+rmdir /sabotage/bin /sabotage/etc /sabotage/dev /sabotage/proc /sabotage/sys
+
+mv -f /sabotage/* /
+rmdir /sabotage
