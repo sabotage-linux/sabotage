f7baa9522c861035d74ed4a2ce40eb575c0bb89b
David A Roberts <d@vidr.cc>
2024-05-16 18:19:15 +1000

    Transition to stage1 bootstrap

--- utils/boot-stage0.sh
+++ utils/boot-stage0.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+ARCH=$(uname -m)
+MAKE_THREADS=$(nproc)
+
 cd /root
 
 make gawk binutils bzip2 curl linux-headers
@@ -13,12 +16,12 @@ cd /sabotage/src
 cp KEEP/config.stage0 config
 sed -e "s@SABOTAGE_BUILDDIR=.*@SABOTAGE_BUILDDIR=/sabotage@" \
     -e "s@CC=.*@CC=cc@" -e "s@HOSTCC=.*@HOSTCC=cc@" \
-    -e "s@MAKE_THREADS=1@MAKE_THREADS=$(NPROC)@" \
+    -e "s@MAKE_THREADS=1@MAKE_THREADS=$MAKE_THREADS@" \
     -i config && cat config
 
 utils/setup-rootfs.sh
 
 CONFIG=/sabotage/src/config BUTCHDB=/sabotage/var/lib/butch.db DEPS=build:stage0 KEEP/bin/butch install stage0
 
-echo "export A=$(ARCH)" > config && cat KEEP/config.stage1 >> config
-sed -i "s@MAKE_THREADS=1@MAKE_THREADS=$(NPROC)@" config && cat config
+echo "export A=$ARCH" > config && cat KEEP/config.stage1 >> config
+sed -i "s@MAKE_THREADS=1@MAKE_THREADS=$MAKE_THREADS@" config && cat config
